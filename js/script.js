baa1ff1d6bc03ac19847ca53c56371a4f4f93e5b
Cris-NTL <natolicristian92@gmail.com>
2023-03-31 13:21:50 +0200

    Static structure

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+
+            contacts: [
+                {
+                    img: '',
+                    name: '',
+                    text: '',
+                    time: ''
+                }, {
+                    img: '',
+                    name: '',
+                    text: '',
+                    time: ''
+                },
+                {
+                    img: '',
+                    name: '',
+                    text: '',
+                    time: ''
+                },
+                {
+                    img: '',
+                    name: '',
+                    text: '',
+                    time: ''
+                },
+                {
+                    img: '',
+                    name: '',
+                    text: '',
+                    time: ''
+                },
+                {
+                    img: '',
+                    name: '',
+                    text: '',
+                    time: ''
+                },
+                {
+                    img: '',
+                    name: '',
+                    text: '',
+                    time: ''
+                },
+                {
+                    img: '',
+                    name: '',
+                    text: '',
+                    time: ''
+                },
+            ],
+        };
+    },
+
+    methods: {
+
+    }
+}).mount("#app");
