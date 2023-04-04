74d8efe5a691ec3467fdb6eacc71d33507f492aa
Cris-NTL <natolicristian92@gmail.com>
2023-04-04 14:19:01 +0200

    Add: luxon

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 const { createApp } = Vue;
 
+const now = luxon.DateTime.now();
+
+const timeNow = now.toFormat("dd/MM/yyyy hh:mm:ss")
+console.log(timeNow);
+const messageDate = luxon.DateTime.fromFormat("10/01/2020 15:30:55", "dd/MM/yyyy hh:mm:ss")
+console.log(messageDate.toFormat("HH:mm"));
+
 createApp({
     data() {
         return {
@@ -186,7 +193,7 @@ createApp({
         sendMessage() {
             if (this.newMessage) {
                 this.selectedContact.messages.push({
-                    date: '',
+                    date: luxon.DateTime.now().toFormat("dd/MM/yyyy hh:mm:ss"),
                     message: this.newMessage,
                     status: 'sent',
                 });
@@ -197,7 +204,7 @@ createApp({
                 //timeout function to receive an 'ok!' response
                 setTimeout(() => {
                     this.selectedContact.messages.push({
-                        date: '',
+                        date: luxon.DateTime.now().toFormat("dd/MM/yyyy hh:mm:ss"),
                         message: 'Ok!',
                         status: 'received',
                     });
